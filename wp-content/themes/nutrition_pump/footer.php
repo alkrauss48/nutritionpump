961ddef625c5aed6b2660783951a0390b2d1a9b9
aaron <alkrauss48@gmail.com>
2016-05-21 15:28:56 -0500

    NutritionPump initial commit

---
+++ wp-content/themes/nutrition_pump/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nutrition_pump
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nutrition_pump' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'nutrition_pump' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'nutrition_pump' ), 'nutrition_pump', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
