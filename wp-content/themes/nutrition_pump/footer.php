c97276ca103c5bb2e213e78d2cbd92744f986709
aaron <alkrauss48@gmail.com>
2016-08-08 13:10:40 -0500

    Add sendgrid plugin and handle contact form

--- wp-content/themes/nutrition_pump/footer.php
+++ wp-content/themes/nutrition_pump/footer.php
@@ -3,7 +3,8 @@
         <div class="inner">
           <section>
             <h2>Get in touch</h2>
-            <form method="post" action="#">
+            <form method="post" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>">
+              <input type="hidden" name="action" value="contact_form">
               <div class="field half first">
                 <input type="text" name="name" id="name" placeholder="Name" />
               </div>
@@ -21,11 +22,11 @@
           <section>
             <h2>Follow</h2>
             <ul class="icons">
-              <li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
-              <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
-              <li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
-              <li><a href="#" class="icon style2 fa-phone"><span class="label">Phone</span></a></li>
-              <li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+              <li><a href="https://twitter.com/nutrition_pump" target="_blank" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+              <!-- <li><a href="#" target="_blank" class="icon style2 fa&#45;facebook"><span class="label">Facebook</span></a></li> -->
+              <li><a href="https://www.instagram.com/nutrition_pump/" target="_blank" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+              <!-- <li><a href="#" target="_blank" class="icon style2 fa&#45;phone"><span class="label">Phone</span></a></li> -->
+              <li><a href="mailto:nutritionpumpokc@gmail.com" target="_blank" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
             </ul>
           </section>
           <ul class="copyright">
