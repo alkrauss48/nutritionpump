b5f38579935a548a71ed2655ce4b389f6c8ad744
aaron <alkrauss48@gmail.com>
2018-01-07 21:23:14 -0600

    Implement contact form 7

--- wp-content/themes/nutrition_pump/footer.php
+++ wp-content/themes/nutrition_pump/footer.php
@@ -3,21 +3,7 @@
         <div class="inner">
           <section>
             <h2>Get in touch</h2>
-            <form method="post" action="">
-              <input type="hidden" name="action" value="contact_form">
-              <div class="field half first">
-                <input type="text" name="name" id="name" placeholder="Name" />
-              </div>
-              <div class="field half">
-                <input type="email" name="email" id="email" placeholder="Email" />
-              </div>
-              <div class="field">
-                <textarea name="message" id="message" placeholder="Message"></textarea>
-              </div>
-              <ul class="actions">
-                <li><input type="submit" value="Send" class="special" /></li>
-              </ul>
-            </form>
+            <?php echo do_shortcode('[contact-form-7 id="49" title="Contact form 1"]') ?>
           </section>
           <section>
             <h2>Follow</h2>
